6cf93ad1d7fde5e1075f60e3c335875e12684584
Oleksandr Volokhov <Flatroniym@gmail.com>
2026-01-15 09:59:34 +0100

    completely made hw number 8

--- src/js/task-1.js
+++
@@ -1,7 +0,0 @@
-const list = document.querySelector("#categories");
-const items = document.querySelectorAll(".item");
-console.dir(`Number of categories ${list.children.length}`);
-items.forEach(a => {
-	console.log(`Category: ${a.firstElementChild.textContent}`);
-	console.log(`Elements: ${a.lastElementChild.children.length}`);
-});
--- src/js/task-3.js
+++
@@ -1,9 +0,0 @@
-const input = document.getElementById('name-input');
-const output = document.getElementById('name-output');
-input.addEventListener('input', (e) => {
-	const inputValue = e.currentTarget.value.trim();
-	output.textContent = inputValue;
-	if (!inputValue) {
-		output.textContent = "Anonymous";
-	}
-});
